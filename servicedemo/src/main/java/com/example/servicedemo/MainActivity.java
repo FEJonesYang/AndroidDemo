923251591c55213d635e6c79637a134abdbf46d2
Yangchao <545661033@qq.com>
2020-12-17 14:52:57 +0800

    绑定服务实现进度监控

--- servicedemo/src/main/java/com/example/servicedemo/MainActivity.java
+++ servicedemo/src/main/java/com/example/servicedemo/MainActivity.java
@@ -7,16 +7,22 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 
 import com.example.servicedemo.services.MyService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivity";
+    /**
+     *  用于绑定客户端和服务
+     */
     ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-
+            MyService.MyBinder myBinder = (MyService.MyBinder) service;
+            Log.d(TAG, String.valueOf(myBinder.getProgress()));
         }
 
         @Override
--- servicedemo/src/main/java/com/example/servicedemo/services/MyService.java
+++ servicedemo/src/main/java/com/example/servicedemo/services/MyService.java
@@ -7,37 +7,59 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+
+    private int mI;
+
     public MyService() {
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("TAG","Service create");
+        Log.d("TAG", "Service create");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (mI = 0; mI < 100; mI++) {
+                    try {
+                        Thread.sleep(300);
+//                        Log.d("TAG", String.valueOf(mI));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("TAG","Service start");
+        Log.d("TAG", "Service start");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        Log.d("TAG","Service bind");
-        return new Binder();
+        Log.d("TAG", "Service bind");
+        return new MyBinder();
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.d("TAG","Service unbind");
+        Log.d("TAG", "Service unbind");
         return super.onUnbind(intent);
     }
 
     @Override
     public void onDestroy() {
-        Log.d("TAG","Service destroy");
+        Log.d("TAG", "Service destroy");
         super.onDestroy();
+    }
+
+    public class MyBinder extends Binder {
+        public int getProgress() {
+            return mI;
+        }
     }
 }
