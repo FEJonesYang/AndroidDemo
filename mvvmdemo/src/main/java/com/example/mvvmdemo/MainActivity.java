b3bbba3c07ab69eeef5c8b1d41e7cd252d65304b
Yangchao <545661033@qq.com>
2021-02-05 10:56:17 +0800

    nothing

--- mvvmdemo/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ mvvmdemo/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -1,22 +1,19 @@
 package com.example.mvvmdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingComponent;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
-
-import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author JonesYang
+ * @Data 2021-01-24
+ * @Function
+ */
 public class MainActivity extends AppCompatActivity {
-
-    private ViewDataBinding mBinding;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // DataBinding 在 Activity 中的使用
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+        setContentView(R.layout.activity_main);
     }
 }
